0c0f45516a73563dd32787ca474b2ca349b24768
Ankitraj7764 <96444056+Ankitraj7764@users.noreply.github.com>
2022-04-24 18:00:57 +0530

    Update Slide.js

--- src/Component/Slide.js
+++ src/Component/Slide.js
@@ -10,13 +10,13 @@ function Slide(){
   <div className="carousel-inner">
   <a class="navbar-brand" href="#">Some Polular Movies</a>
     <div className="carousel-item active">
-      <img src="friends.jpg" className="d-block w-100" alt="..."/>
+      <img src={require('./joker1.jpg')} />
     </div>
-    <div className="carousel-item">
-      <img src="jersey11.jpg" className="d-block w-100" alt="..."/>
+    <div class="carousel-item">
+    <img src={require('./jersey11.jpg')} />
     </div>
-    <div className="carousel-item">
-      <img src="joker1.jpg" className="d-block w-100" alt="..."/>
+    <div class="carousel-item">
+    <img src={require('./friends.jpg')} />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
